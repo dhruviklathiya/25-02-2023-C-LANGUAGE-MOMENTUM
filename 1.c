3d56345011f0e0bd5044c2e44593bf2ef8eeb722
dhruviklathiya <115994798+dhruviklathiya@users.noreply.github.com>
2023-03-02 21:03:55 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
 int main()
 {
-    int a;
-    (a / 2 == 0)? 
-    printf("Number is even");
-    :
-    printf("ncjdncjdnc");
+    char a;
+    printf("ENTER CHARACTER: ");
+    scanf("%c", &a);
+    if((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+    {
+        printf("'%c' IS ALPHABET", a);
+    }
+    else if(a >= '0' && a <= '9')
+    {
+        printf("'%c' IS DIGIT", a);
+    }
+    else 
+    {
+        printf("'%c' IS SPECIAL CHARACTER", a);
+    }
     return 0;
 }
